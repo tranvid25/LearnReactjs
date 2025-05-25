65037de34c57a6f195121240521d363476abb09d
tranvid25 <lsdnh041004@gmail.com>
2025-05-25 20:39:23 +0700

    vd_useRef

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Product from "./components/Product";
 import Category from "./components/category";
 import Not_Category from "./components/Not_Category";
@@ -7,37 +7,24 @@ import Content from "./components/Content";
 import Timer from "./components/Timer";
 
 function App() {
-const[count,setCount]=useState(1);
-const[post,setPost]=useState([]);
-const[color,setColor]=useState("");
-const[show,setShow]=useState(false);
-// useEffect(()=>{
-//   fetch("http::")
-//   .then((response) =>response.json())
-//   .then((json)=>setPost(json));
-// });
-useEffect(()=>{
- if(count%2==0){
-  setColor("red");
- }else{
-  setColor("black")
- }
-},[count]);
+const [count,setCount]=useState(0);
+const timerId=useRef();
+const handleStart=()=>{
+ timerId.current= setInterval(()=>{
+    setCount((prev)=>prev+1);
+  },1000);
+}
+const handleStop=()=>{
+  clearInterval(timerId.current);
+};
 return (
    <div>
-    <div>{count}</div>
-    <div style={{background:color,
-      width:"70px",
-      height:"40px"
-    }}>
-
-    </div>
-   <button onClick={()=>setCount(count+1)}>Click me</button>
-   <button onClick={()=>setShow(!show)}>Show</button>
-   {show&&<Timer/>}
-   <Content/>
+   <h2>{count}</h2>
+    <button onClick={handleStart}>Start</button>
+    <button onClick={handleStop}>Stop</button>
    </div>
   );
 }
 
 export default App;
+//render là hiển thị giao diện ra màn hình 
--- react-project/src/components/Content.jsx
+++ react-project/src/components/Content.jsx
@@ -7,6 +7,8 @@ export default function Content(){
             setCount(0);
         }
     },[count]);
+    // ví dụ như layout effect khi chạy xong nó sẽ thực thi xong rồi ms hiển thị ra màn hình 
+    // còn efect sẽ thực thi ra màn hình 
     //dùng để chạy đoạn code sau khi component
     // nếu count thay đổi tới một điều kiện nhất định thì useEffect sẽ thực thi để cập nhật
     const handleIncrease=(()=>{

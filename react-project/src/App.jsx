5c76ed45bd2817fcaae9980e9541cbb99908e411
tranvid25 <lsdnh041004@gmail.com>
2025-05-25 17:11:10 +0700

    todolist

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -6,14 +6,63 @@ import Message from "./components/Message";
 import Content from "./components/Content";
 
 function App() {
-const[show,SetShow]=useState(false);
-const handleShow=() =>{
-   SetShow(!show);
+const [todo,setTodo]=useState({
+  id:"",
+  name:""
+});
+//tạo ra setTodo để cập nhật giá trị
+const [todoList,setTodoList]=useState([
+  {id:1,name:"danh rang rua mat"},
+]);
+const handleGetTodo=(value)=>{
+   const randomId=Math.random(10000);
+   const todo={
+    id:randomId,
+    name:value,
+   };
+   setTodo(todo);
 };
+//cái này dùng để tạo giá trị todo từ value nhập vào là name và id là randomId
+//sau đó xét giá trị todo vào settoto để cập nhật
+
+const handleSubmit=()=>{
+console.log(todo);
+setTodoList((prev)=>{
+  return [
+    ...prev,todo
+  ]
+});
+setTodo({
+  id:"",
+  name:""
+});
+//khi người dùng click vào thì settodollist sẽ vẫn giữ giá trị trc đó và cập nhật thêm giá trị mới vào
+}
+const handledelete=(id)=>{
+  setTodoList(todoList.filter(todo =>todo.id !== id));
+  //filter dùng để lọc các điều kiện cho trước trả ề 1 mảng mới
+}
   return (
     <div>
-     <button onClick={handleShow}>Show</button>
-      {show && <Content/>}
+     <div>
+      <input type="text" onChange={(e)=>handleGetTodo(e.target.value)} 
+      value={todo.name} />
+      <button onClick={handleSubmit}>Add</button>
+     </div>
+     <div>
+       <ul>
+        {todoList.map((todo,index)=>{
+          return(
+            <div style={{display:"flex",
+              gap:"30px"
+            }} key={index}>
+              <li>{todo.name}</li>
+              <div style={{cursor:"pointer"}} onClick={()=>handledelete(todo.id)}>Xóa</div>
+            </div>
+          );
+        })}
+       </ul>
+     </div>
     </div>
   );
 }

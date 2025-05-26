66a114a40cdb0fcf4490d123dd336cdc5b53f511
tranvid25 <lsdnh041004@gmail.com>
2025-05-26 20:46:24 +0700

    useMemo

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Product from "./components/Product";
 import Category from "./components/category";
 import Not_Category from "./components/Not_Category";
@@ -7,14 +7,41 @@ import Content from "./components/Content";
 import Timer from "./components/Timer";
 
 function App() {
-const [count,setCount]=useState(0);
-const handleIncrease=useCallback(()=>{
-  setCount((prev)=>prev+1);
-},[]);
+const[courses,setCourses]=useState([]);
+const [name,setName]=useState("");
+const[price,setPrice]=useState("");
+const handlesubmit=()=>{
+  const course={
+    name,
+    price:+price,
+  };
+  setCourses((prev)=>[...prev,course])
+};
+// const total=courses.reduce((cur,course)=>{
+//    return cur+course.price;
+// },0);
+const total=useMemo(()=>{
+ return courses.reduce((cur,course)=>{
+    return cur+course.price;
+  },0);
+},[courses])
 return (
    <div>
-     <div>{count}</div>
-     <Content handleIncrease={handleIncrease}/>
+    <div>
+      <input type="text"placeholder="Nhập tên khóa học" value={name} onChange={(e)=>setName(e.target.value)}/>
+      <input type="text"placeholder="Nhập giá khóa học" value={price} onChange={(e)=>setPrice(e.target.value)} />
+      <button onClick={handlesubmit}>Add</button>
+      <div>Total:{total}</div>
+      <ul>
+        {courses.map((course,index)=>{
+          return (
+              <li key={index}>
+                Tên khóa học:{course.name},Giá:{course.price}
+              </li>
+          );
+        })}
+      </ul>
+    </div>
    </div>
   );
 }

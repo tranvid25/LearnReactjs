c9bc3bbb6162df36769c6c6a30b3d65fac9f9a2f
tranvid25 <lsdnh041004@gmail.com>
2025-05-26 22:41:55 +0700

    DOM

---
+++ learnDOM/main.js
@@ -0,0 +1,24 @@
+//html dom là document object model
+// Có 3 thành phàn 
+/**
+ * 1.Element là thẻ tag 
+ * 2.Atribute class id ,src đồ 
+ * 3.text là chữ
+ */
+
+//js:Browser|Server
+
+//Browser:HTML->DOM->DOM API
+
+// document.write('nhận vào chuỗi');
+ var heading=document.querySelector('.box.heading-2');
+ console.log(heading);
+/**
+1.getElementById
+2.getElementsByClassName
+3.getElementsByTagName
+4.querySelector
+5.querySelectorAll
+6.HTML collection
+7.document.write
+ */

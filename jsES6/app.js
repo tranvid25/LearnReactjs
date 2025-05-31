06b51889b8753b31e98b5158e204fc2fac7a7228
tranvid25 <lsdnh041004@gmail.com>
2025-05-31 16:59:37 +0700

    comment

--- jsES6/app.js
+++ jsES6/app.js
@@ -78,3 +78,77 @@ const logger =(log)=>{
 logger('Message...');
 const sum =(a,b)=>a+b;
 console.log(sum(2,2));
+
+var users=[
+    {
+        id:1,
+        name:'Tran Vi',
+    },
+    {
+        id:2,
+        name:'lac ga'
+    },
+    {
+        id:3,
+        name:'Le hien'
+    },
+];
+var comments=[
+    {
+        id:1,
+        user_id:1,
+        content:'i am ronaldo'
+    },
+    {
+        id:2,
+        user_id:2, 
+        content:'Kệ mẹ m'
+    }
+];
+//1.Láy comments
+//2.Từ comments lấy ra user_id,
+//từ user_id lấy ra user tương ứng
+
+//Faake API
+function getComment(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(comments);
+        },1000);
+})
+}
+function getUserById(userIds){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            var result = users.filter(function(user){
+                return userIds.includes(user.id);
+            });
+            resolve(result);
+        }, 1000);
+    });
+}
+
+getComment()
+.then(function(comments){
+   var userIds=comments.map(function(comment){//lấy userId
+      return comment.user_id;
+   });
+   return getUserById(userIds).then(function(users){
+    return {
+        users:users,
+        comments:comments,
+    };
+   });
+})
+.then(function(data){
+    var commentBlock=document.getElementById('comment-block');
+    var html='';
+    data.comments.forEach(function(comment){
+       var user=data.users.find(function(user){
+        return user.id===comment.user_id
+       });
+       html+=`<li>${user.name}:${comment.content}</li>`
+
+    });
+    commentBlock.innerHTML=html;
+})
